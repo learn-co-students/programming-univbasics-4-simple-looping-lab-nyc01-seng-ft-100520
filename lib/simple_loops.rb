d6ddabcaa6290402a8f1768c6dd03dd905d223a4
Christopher Clark <cclark000@citymail.cuny.edu>
2020-09-11 15:38:11 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,31 @@
-# Write your methods here
+def loop_message_five_times (message)
+  5.times do 
+    puts message
+  end
+end
+
+
+def loop_message_n_times (message, n)
+  n.times do
+    puts message
+  end
+end
+
+
+def output_array (array)
+  counter = 0
+  
+  while array[counter] do
+    puts array[counter]
+    counter += 1
+  end
+end
+
+
+def return_string_array (array)
+  counter = 0
+  
+  while array[counter]
+    array.to_s[counter]
+  end
+end
